0e33ddf7148953178cacf80f88feb9cc28b906f1
smie15352353 <xiaoshq6@mail2.sysu.edu.cn>
2017-10-08 21:54:46 +0800

    Add SecondActivity

---
+++ app/src/main/java/com/xiaoshq/infosystem/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.xiaoshq.infosystem;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by xiaoshq on 2017/10/8.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+}
